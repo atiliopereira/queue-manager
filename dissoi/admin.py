b119e933ba424ad71dc5b6eae58af9f32031c1ec
Atilio Pereira <atilio@dissoi.com>
2017-08-05 14:56:00 -0400

    CREDI-11 Convervar logueado al usuario al cerrar la ventana del navegador

--- dissoi/admin.py
+++ dissoi/admin.py
@@ -5,13 +5,12 @@ from dissoi.models import Sesion
 
 @admin.register(Sesion)
 class SesionAdmin(admin.ModelAdmin):
-    list_display = ['funcionario','fecha_login','hora_logout']
-    readonly_fields = ['funcionario','fecha_login']
-    fields = ['funcionario','fecha_login','hora_logout']
+    list_display = ['funcionario', 'fecha_login', 'hora_logout']
+    readonly_fields = ['funcionario', 'fecha_login']
+    fields = ['funcionario', 'fecha_login', 'hora_logout']
 
     def has_add_permission(self, request):
         return False
-
 
     def has_delete_permission(self, request, obj=None):
         return False
